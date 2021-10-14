8af860ce9335738c20ce5eb3f9e2794623d9c051
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2021-10-14 14:47:42 +0900

    Implemnented Drawer component's property.

---
+++ react-frontend/src/components/drawer/props.ts
@@ -0,0 +1,54 @@
+/**
+ * @fileoverview Defines {Drawer} and {Item} properties.
+ */
+import type * as React from "react";
+
+/** A type union of HTML container getters. */
+type GetContainer = string | HTMLElement | (() => HTMLElement);
+
+/** A type union of CSS position properties. */
+type Placement = "left" | "top" | "right" | "bottom";
+
+/** A type union of draw width definitions. */
+type DrawWidth = number | [number, number];
+
+/** A common properties shared by both {Drawer} and {Item} components. */
+interface CommonProps extends Omit<React.HTMLAttributes<unknown>, "onChange"> {
+  prefixClass?: string;
+  width?: string | number;
+  height?: string | number;
+  open?: boolean;
+  defaultOpen?: boolean;
+  handler?: React.ReactElement | null | false;
+  placement?: Placement;
+  drawLevel?: null | string | string[];
+  drawWidth?:
+    | DrawWidth
+    | ((e: { target: HTMLElement; open: boolean }) => DrawWidth);
+  drawDuration?: string;
+  drawEase?: string;
+  showMask?: boolean;
+  maskClosable?: boolean;
+  maskStyle?: React.CSSProperties;
+  onChange?: (open?: boolean) => void;
+  afterVisibleChange?: (open: boolean) => void;
+  onHandleClick?: (e: React.MouseEvent | React.KeyboardEvent) => void;
+  onClose?: (e: React.MouseEvent | React.KeyboardEvent) => void;
+  keyboard?: boolean;
+  contentWrapperStyle?: React.CSSProperties;
+  autoFocus?: boolean;
+}
+
+/** A {Drawer} component properties. */
+export interface DrawerProps extends CommonProps {
+  getContainer?: GetContainer;
+  wrapperClass?: string;
+  forceRender?: boolean;
+}
+
+/** A {Item} component properties. */
+export interface ItemProps extends CommonProps {
+  getContainer?: GetContainer;
+  getOpenCount?: () => number;
+  switchScrollingEffect?: () => void;
+}
