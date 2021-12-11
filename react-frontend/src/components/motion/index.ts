e2a0b5dcec42c1daf0a79c43421771939855af46
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2021-12-11 13:40:56 +0900

    implement motion component.

--- react-frontend/src/components/motion/index.ts
+++ react-frontend/src/components/motion/index.ts
@@ -14,3 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+export type {
+  Motion as Props,
+} from "./props";
+
+export {
+  ExoticComponent as Component,
+} from './motion';
