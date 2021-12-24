f9bc94d55be79c17ad232aafe94f5305e12047ad
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2021-12-25 04:26:03 +0900

    add some package managers.

--- SLAMCamera/SLAMCamera/ContentView.swift
+++ SLAMCamera/SLAMCamera/ContentView.swift
@@ -3,15 +3,15 @@
 //  SLAMCamera
 //
 //  Created by Shingo OKAWA on 2021/09/05.
+//  Copyright © 2021 Shingo OKAWA. All rights reserved.
 //
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
