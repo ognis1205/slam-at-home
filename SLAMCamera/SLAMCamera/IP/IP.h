2073c4fec033f808aade5efbd58afd523e02ddeb
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2021-12-26 00:29:12 +0900

    Implement IP module.

---
+++ SLAMCamera/SLAMCamera/IP/IP.h
@@ -0,0 +1,18 @@
+//
+//  IP.h
+//  SLAMCamera
+//
+//  Created by Shingo OKAWA on 2021/12/24.
+//  Copyright © 2021 Shingo OKAWA. All rights reserved.
+//
+#ifndef IP_h
+#define IP_h
+  // Placeholder
+#endif /* IP_h */
+
+#import <Foundation/Foundation.h>
+
+@interface IP : NSObject
+/// Returns the device IP address.
++ (NSString *) get;
+@end
