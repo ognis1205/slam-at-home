32cb0f70aee3f53ca2a0e5763e68496a51a2c2c4
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2021-12-26 00:29:26 +0900

    Implement IP module.

---
+++ SLAMCamera/SLAMCamera/IP/IP.m
@@ -0,0 +1,33 @@
+//
+//  IP.m
+//  SLAMCamera
+//
+//  Created by Shingo OKAWA on 2021/12/24.
+//  Copyright © 2021 Shingo OKAWA. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+#include <ifaddrs.h>
+#include <arpa/inet.h>
+#include "IP.h"
+
+@implementation IP
+/// Returns the device IP address.
++ (NSString *) get {
+  NSString *address = @"ip_address_error";
+  struct ifaddrs *interfaces = NULL;
+  struct ifaddrs *addr = NULL;
+  if (getifaddrs(&interfaces) == 0) {
+    addr = interfaces;
+    while (addr != NULL) {
+      if(addr->ifa_addr->sa_family == AF_INET) {
+        if ([[NSString stringWithUTF8String:addr->ifa_name] isEqualToString:@"en0"]) {
+          address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)addr->ifa_addr)->sin_addr)];
+        }
+      }
+      addr = addr->ifa_next;
+    }
+  }
+    freeifaddrs(interfaces);
+    return address;
+}
+@end
