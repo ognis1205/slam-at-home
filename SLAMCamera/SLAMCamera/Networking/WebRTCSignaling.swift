37ce17c0f628a3f5b343769413847c08c0e107e1
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2021-12-31 03:02:32 +0900

    Start implementing WebRTC service.

---
+++ SLAMCamera/SLAMCamera/Networking/WebRTCSignaling.swift
@@ -0,0 +1,61 @@
+//
+//  HttpLiveStreaming.swift
+//  SLAMCamera
+//
+//  Created by Shingo OKAWA on 2021/12/30.
+//
+import Foundation
+import WebRTC
+
+enum Message {
+  case sdp(SessionDescription)
+  case candidate(IceCandidate)
+}
+
+extension Message: Codable {
+  /// Initializer.
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    let type = try container.decode(String.self, forKey: .type)
+    switch type {
+      case String(describing: SessionDescription.self):
+        self = .sdp(try container.decode(SessionDescription.self, forKey: .payload))
+      case String(describing: IceCandidate.self):
+        self = .candidate(try container.decode(IceCandidate.self, forKey: .payload))
+      default:
+        throw DecodeError.unknownType
+    }
+  }
+
+  /// Encodes SDP/ICE candidates.
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    switch self {
+      case .sdp(let sessionDescription):
+        try container.encode(sessionDescription, forKey: .payload)
+        try container.encode(String(describing: SessionDescription.self), forKey: .type)
+      case .candidate(let iceCandidate):
+        try container.encode(iceCandidate, forKey: .payload)
+        try container.encode(String(describing: IceCandidate.self), forKey: .type)
+    }
+  }
+
+  enum DecodeError: Error {
+    case unknownType
+  }
+    
+  enum CodingKeys: String, CodingKey {
+    case type, payload
+  }
+}
+
+public protocol WebRTCSignaling: AnyObject {
+  ///  Connects to the signaling server.
+  func connect() -> Void
+
+  /// Sends WebRTC SDP to the server.
+  func send(sdp rtcSdp: RTCSessionDescription) -> Void
+
+  /// Sends WebRTC ICE candidate to the server.
+  func send(candidate rtcIceCandidate: RTCIceCandidate) -> Void
+}
