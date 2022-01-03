d61e45f636b612309fa680798f9cf45e6bfca372
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2022-01-04 00:35:47 +0900

    updated dev environment.

--- mlmodels/pydnet/setup.py
+++ mlmodels/pydnet/setup.py
@@ -1,10 +1,24 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
+from pkg_resources import parse_requirements
+
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+    print(required)
+
 
 setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
-    description='PydNet: Single inference using TensorFlow 1.15 and python 3.x.',
-    author='Shingo OKAWA',
-    license='',
-)
+    name="pydnet",
+    version="1.0.0",
+    description="PydNet: Single inference using TensorFlow 2 and python 3.x.",
+    author="Shingo OKAWA",
+    python_requires=">=3.5",
+    install_requires=required,
+    extras_require = {
+        "test": [
+            "pytest",
+            "notebook"
+        ]
+    },
+    packages=find_packages(exclude=["test", "test.*"]),
+    test_suite="test")
