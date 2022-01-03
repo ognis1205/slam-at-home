b5229fd5b2442600b4f9dadb1103bc2e437bf214
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2022-01-03 10:42:20 +0900

    start implementing pydnet.

---
+++ mlmodels/pydnet/pydnet/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='PydNet: Single inference using TensorFlow 1.15 and python 3.x.',
+    author='Shingo OKAWA',
+    license='',
+)
