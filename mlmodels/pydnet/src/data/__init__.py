3e6fdfe283c56f556e5a8882247dd1c82420ebbd
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2022-01-06 23:30:24 +0900

    implement dataset loader.

--- mlmodels/pydnet/src/data/__init__.py
+++ mlmodels/pydnet/src/data/__init__.py
@@ -1,9 +1,11 @@
 """Data Module.
 """
 
+from src.data.kitti import KITTI
 from src.data.kitti_utils import generate_depth_map
 
 
 __all__ = [
+    "KITTI",
     "generate_depth_map"
 ]
---
+++ mlmodels/pydnet/src/data/kitti.py
@@ -0,0 +1,57 @@
+"""Heap Module.
+"""
+
+import tensorflow.compat.v1 as tf1
+import tensorflow as tf2
+import numpy as np
+import numpy.typing as npt
+
+
+class KITTI(object):
+    """KITTI-360 dataset loader.
+    """
+
+    def __init__(self, params: dict):
+        """Inits `KITTI` with `params`.
+        """
+        self.workers = 4
+        self.h = params["h"]
+        self.w = params["w"]
+        self.path = params["path"]
+        self.slice = np.loadtxt(params["slice"], dtype=bytes).astype(np.str)
+        self._build()
+
+    def _build(self) -> None:
+        """Builds KITTI dataset from a given `params`.
+        """
+        dataset = tf1.data.Dataset.from_tensor_slices(
+            tf1.convert_to_tensor(self.slice, dtype=tf1.string))
+        dataset = dataset.map(
+            self._reshape,
+            num_parallel_calls=self.workers)
+        dataset = dataset.batch(1)
+        dataset = dataset.repeat()
+        iterator = dataset.make_initializable_iterator()
+        self.initializer = iterator.initializer
+        self.batch = iterator.get_next()
+
+    def _imread(self, filename: str) -> np.ndarray:
+        """Read jpeg file from file system.
+        """
+        img = tf1.image.decode_jpeg(
+            tf1.io.read_file(str(self.path.resolve() / f"{filename}.jpg")),
+            channels=3)
+        img = tf1.cast(img, tf1.float32)
+        return img
+
+    def _reshape(self, filename: str) -> np.ndarray:
+        """Prepare single image at testing time.
+        """
+        img = self._imread(filename)
+        img = tf1.image.resize_images(
+            img,
+            [self.h, self.w],
+            tf1.image.ResizeMethod.AREA)
+        img.set_shape([self.h, self.w, 3])
+        img = img / 255.0
+        return img
