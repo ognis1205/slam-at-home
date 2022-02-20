36306add0a41d8093ab28585cf186470291436ac
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2022-02-20 15:12:17 +0900

    add some scripts.

---
+++ mlmodels/pydnet/pydnet/cli/update_mlmodel.py
@@ -0,0 +1,39 @@
+"""MLModel Updater CLI.
+"""
+
+import sys
+import fire
+import coremltools as ct
+import coremltools.proto.FeatureTypes_pb2 as ft
+from traceback import format_exc
+from pathlib import Path
+
+def update_mlmodel(path_to_mlmodel, path_to_output):
+    """Updates the PyDnet model.
+    Args:
+        path_to_mlmodel (str): The path to the mlmodel file.
+        path_to_output (str): The path to the output directory.
+    """
+    spec = ct.utils.load_spec(path_to_mlmodel)
+    for output in spec.description.output:
+        shape = tuple(output.type.multiArrayType.shape)
+        c, h, w = shape
+        output.type.imageType.colorSpace = ft.ImageFeatureType.ColorSpace.Value(
+            "GRAYSCALE"
+        )
+        output.type.imageType.width  = w
+        output.type.imageType.height = h
+    updated = ct.models.MLModel(spec)
+    updated.author  = "Filippo Aleotti"
+    updated.license = "Apache v2"
+    updated.short_description = "pydnet"
+    updated.save(path_to_output)
+
+
+def main():
+    """A CLI entry point.
+    """
+    try:
+        fire.Fire(update_mlmodel)
+    except Exception:
+        print(format_exc(), file=sys.stderr)
