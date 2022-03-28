41deb3287432b8962961a7d4049bae8a8ca75759
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2022-03-28 13:32:07 +0900

    implement wip page.

---
+++ react-frontend/src/containers/maintenance/index.ts
@@ -0,0 +1,5 @@
+/**
+ * @fileoverview Defines {Maintenance} module.
+ * @copyright Shingo OKAWA 2022
+ */
+export { Component } from './wip';
---
+++ react-frontend/src/containers/maintenance/wip.tsx
@@ -0,0 +1,30 @@
+/**
+ * @fileoverview Defines WIP component.
+ * @copyright Shingo OKAWA 2022
+ */
+import * as React from 'react';
+import * as Props from './props';
+import styles from '../../assets/styles/containers/maintenance.module.scss';
+
+/** Returns a `WIP` component. */
+export const Component: React.FunctionComponent<
+  React.HTMLAttributes<HTMLDivElement>
+> = (props: Props.Github): React.ReactElement => {
+  return (
+    <div {...props} className={styles['wrapper']}>
+      <div className={styles['animation']} />
+      <section className={styles['content']}>
+        <h1>Pardon my dust.</h1>
+        <p>
+          The page is currently WIP and is planned to deliver in the near future.
+        </p>
+        <footer>
+          If you need to you can always <a href="mailto:shingo.okawa.g.h.c@gmail.com">contact me</a>.
+        </footer>
+      </section>
+    </div>
+  );
+};
+
+/** Sets the component's display name. */
+Component.displayName = 'WIP';
