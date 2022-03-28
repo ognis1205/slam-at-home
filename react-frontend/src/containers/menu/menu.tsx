b310fcdc80caa306ba602f8ac986d81b5631dec5
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2022-03-29 01:17:13 +0900

    implement menu.

--- react-frontend/src/containers/menu/menu.tsx
+++ react-frontend/src/containers/menu/menu.tsx
@@ -6,8 +6,33 @@ import * as React from 'react';
 import * as Props from './props';
 import * as Modal from '../../components/modal';
 import * as Hook from '../../utils/hook';
+import * as FontAwesome from '@fortawesome/react-fontawesome';
+import * as FontAwesomeCore from '@fortawesome/fontawesome-svg-core';
+import * as FontAwesomeIcon from '@fortawesome/free-solid-svg-icons';
+import * as FontAwesomeBrandIcon from '@fortawesome/free-brands-svg-icons';
 import classnames from 'classnames';
-import styles from '../../assets/styles/components/menu.module.scss';
+import styles from '../../assets/styles/containers/menu.module.scss';
+
+/** Returns the corresponding FontAwesome icon. */
+const getIcon = (type: Props.ItemType): FontAwesomeCore.IconDefinition => {
+  switch (type) {
+    case Props.ItemType.SETTING:
+      return FontAwesomeIcon.faBars;
+    case Props.ItemType.GITHUB:
+      return FontAwesomeBrandIcon.faGithub;
+    case Props.ItemType.GITTER:
+      return FontAwesomeBrandIcon.faGitter;
+    case Props.ItemType.SHARE:
+      return FontAwesomeIcon.faShareSquare;
+    case Props.ItemType.INFO:
+    default:
+      return FontAwesomeIcon.faInfo;
+  }
+};
+
+/** Returns the class name of the icon. */
+const getIconClassName = (type: Props.ItemType): string =>
+  classnames(styles['icon'], styles[type]);
 
 /** Returns the class name of the content. */
 const getClassName = (className: string): string =>
@@ -15,16 +40,96 @@ const getClassName = (className: string): string =>
     [className || '']: !!className,
   });
 
+/** Returns a `Container` component. */
+export const Container: React.FunctionComponent<
+  React.HTMLAttributes<HTMLDivElement>
+> = ({
+  children,
+  ...divProps
+}: React.HTMLAttributes<HTMLDivElement>): React.ReactElement => (
+  <div {...divProps}>{children}</div>
+);
+
+/** Sets the component's display name. */
+Container.displayName = 'Controller';
+
+/** Returns a `Divider` component. */
+export const Divider: React.FunctionComponent<
+  React.HTMLAttributes<HTMLDivElement>
+> = ({
+  ...divProps
+}: React.HTMLAttributes<HTMLDivElement>): React.ReactElement => (
+  <div {...divProps} className={styles['divider']} />
+);
+
+/** Sets the component's display name. */
+Container.displayName = 'Controller';
+
+/** Returns a `ExternalLink` component. */
+export const ExternalLink: React.FunctionComponent<Props.ExternalLink> = ({
+  type,
+  title,
+  ...aAttrs
+}: Props.ExternalLink): React.ReactElement => (
+  <div className={styles['item']}>
+    <a className={styles['link']} {...aAttrs}>
+      <span className={getIconClassName(type)}>
+        <FontAwesome.FontAwesomeIcon icon={getIcon(type)} />
+      </span>
+      <span className={styles['title']}>{title}</span>
+    </a>
+  </div>
+);
+
+/** Sets the component's display name. */
+ExternalLink.displayName = 'Externallink';
+
 /** Returns a `Menu` component. */
-export const Component = React.forwardRef<Modal.Trigger, Props.Content>(
+export const Component = React.forwardRef<Props.Trigger, Props.Menu>(
   (
-    { className, ...rest }: Props.Content,
-    ref: React.ForwardedRef<Modal.Trigger>
+    { className, ...rest }: Props.Menu,
+    ref: React.ForwardedRef<Props.Trigger>
   ): React.ReactElement => {
     return (
-      <Modal.Component {...rest} className={getClassName(className)} ref={ref}>
-        Popup content Here
-      </Modal.Component>
+      <Container
+        {...rest}
+        className={getClassName(className)}
+        ref={ref}
+      >
+        <ExternalLink
+          title="Settings"
+          key="settings"
+          type="setting"
+          target="_blank"
+        />
+        <Divider />
+        <ExternalLink
+          title="Report a bug"
+          key="bugreport"
+          type="github"
+          target="_blank"
+          href="https://github.com/ognis1205/slam-at-home/issues"
+        />
+        <ExternalLink
+          title="Get help"
+          key="gethelp"
+          type="gitter"
+          target="_blank"
+        />
+        <ExternalLink
+          title="Share this app"
+          key="share"
+          type="share"
+          target="_blank"
+        />
+        <Divider />
+        <ExternalLink
+          title="About"
+          key="about"
+          type="info"
+          target="_blank"
+        />
+      </Container>
     );
   }
 );
--- react-frontend/src/containers/menu/props.ts
+++ react-frontend/src/containers/menu/props.ts
@@ -3,7 +3,32 @@
  * @copyright Shingo OKAWA 2022
  */
 import type * as React from 'react';
+import type * as Modal from '../../components/modal';
 import type * as Types from '../../utils/types';
+
+/***/
+export type Trigger = Modal.Trigger;
+
+/** A type union of application type properties. */
+export const ItemType = {
+  SETTING: 'setting',
+  GITHUB: 'github',
+  GITTER: 'gitter',
+  SHARE: 'share',
+  INFO: 'info',
+} as const;
+
+export type ItemType = typeof ItemType[keyof typeof ItemType];
+
+/** A {Externallink} component properties. */
+export type ExternalLink = Types.Overwrite<
+  React.AnchorHTMLAttributes<HTMLAnchorElement>,
+  {
+    key: string | number;
+    type: ItemType;
+    title: string;
+  }
+>;
 
 /** A {Menu} component properties. */
 export type Menu = Types.Overwrite<
