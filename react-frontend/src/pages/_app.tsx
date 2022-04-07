f935af4c88bac370a84f126504a7ba42c58dd528
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2022-04-08 05:34:23 +0900

    remove test code

--- react-frontend/src/pages/_app.tsx
+++ react-frontend/src/pages/_app.tsx
@@ -3,82 +3,14 @@
  * @copyright Shingo OKAWA 2021
  */
 import * as React from 'react';
-import * as ReactRedux from 'react-redux';
 import * as NextApp from 'next/app';
 import * as Store from '../redux/store';
-import * as Notifications from '../redux/modules/notifications';
 import * as Panel from '../containers/panel';
 import * as Notification from '../containers/notification';
 import Head from 'next/head';
 import NotificationMotion from '../assets/motions/notification';
 import '../assets/styles/reset.css';
 import '../assets/styles/global.scss';
-
-const Test: React.FunctionComponent = (): React.ReactElement => {
-  /***/
-  const dispatch = ReactRedux.useDispatch();
-
-  return (
-    <p>
-      <button
-        className="btn btn-primary"
-        onClick={() => {
-          dispatch(
-            Notifications.info({
-              title: 'Info',
-              message: 'Information notification message',
-              showCloseButton: true,
-            })
-          );
-        }}
-      >
-        Info
-      </button>
-      <button
-        className="btn btn-primary"
-        onClick={() => {
-          dispatch(
-            Notifications.success({
-              title: 'Success',
-              message: 'Information notification message',
-              showCloseButton: true,
-            })
-          );
-        }}
-      >
-        Success
-      </button>
-      <button
-        className="btn btn-primary"
-        onClick={() => {
-          dispatch(
-            Notifications.warning({
-              title: 'Warning',
-              message: 'Information notification message',
-              showCloseButton: true,
-            })
-          );
-        }}
-      >
-        Warning
-      </button>
-      <button
-        className="btn btn-primary"
-        onClick={() => {
-          dispatch(
-            Notifications.error({
-              title: 'Error',
-              message: 'Information notification message',
-              showCloseButton: true,
-            })
-          );
-        }}
-      >
-        Error
-      </button>
-    </p>
-  );
-};
 
 /** Returns a `SLAM` component. */
 const SLAM: React.FC<NextApp.AppProps> = ({ Component, pageProps }) => (
