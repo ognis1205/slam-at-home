5ed475036386ff7db0879ac4568ea4583adb89db
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2022-04-13 19:03:31 +0900

    refactor webgl module

--- react-frontend/src/components/players/props.ts
+++ react-frontend/src/components/players/props.ts
@@ -3,54 +3,15 @@
  * @copyright Shingo OKAWA 2022
  */
 import type * as React from 'react';
-import type * as THREE from 'three';
 import * as Types from '../../utils/types';
-import * as WebGL from '../../utils/webgl';
 
 /** A {Video} component properties. */
 export type Video = Types.Overwrite<
   React.HTMLAttributes<HTMLDivElement>,
   {
-    stream?: MediaStream;
+    isReady: boolean;
   }
 >;
-
-/** WebGL uniforms for scene. */
-export type SceneUniforms = {
-  colormap: WebGL.Uniform<THREE.Texture>;
-  width: WebGL.Uniform<number>;
-  height: WebGL.Uniform<number>;
-  nearClipping: WebGL.Uniform<number>;
-  farClipping: WebGL.Uniform<number>;
-  pointSize: WebGL.Uniform<number>;
-  zOffset: WebGL.Uniform<number>;
-};
-
-/** WebGL scene properties. */
-export type Scene = Types.Overwrite<
-  SceneUniforms,
-  {
-    fps?: number;
-    vertexShader?: string;
-    fragmentShader?: string;
-  }
->;
-
-/** WebGL camera properties. */
-export type Camera = {
-  fov: number;
-  aspect: number;
-  nearPlane: number;
-  farPlane: number;
-  enableDamping?: boolean;
-  dampingFactor?: number;
-  enableZoom?: boolean;
-  zoomSpeed?: number;
-  enableKeys?: boolean;
-  screenSpacePanning?: boolean;
-  enableRotate?: boolean;
-  enablePan?: boolean;
-};
 
 /** A {Scene} component properties. */
 export type Projector = Types.Overwrite<
--- react-frontend/src/components/players/video.tsx
+++ react-frontend/src/components/players/video.tsx
@@ -4,8 +4,6 @@
  */
 import * as React from 'react';
 import * as Props from './props';
-import * as DOM from '../../utils/dom';
-import * as Hook from '../../utils/hook';
 import classnames from 'classnames';
 import styles from '../../assets/styles/components/players.module.scss';
 
@@ -15,57 +13,20 @@ const getClassName = (className: string): string =>
     [className || '']: !!className,
   });
 
-/** Returns a `Video` component. */
-export const Component: React.FunctionComponent<Props.Video> = ({
-  className,
-  stream,
-  ...divAttrs
-}: Props.Video): React.ReactElement => {
-  /** @const Holds a reference to the video. */
-  const video = React.useRef<HTMLVideoElement>(null);
-
-  const play = (): void => {
-    if (video.current && video.current.srcObject !== stream) {
-      video.current.srcObject = stream;
-
-      video.current.oncanplay = function () {
-        console.log(`video oncanplay`);
-      };
-
-      video.current.onplay = function () {
-        console.log(`video onplay`);
-      };
-
-      video.current.onpause = function () {
-        console.log(`video onpause`);
-      };
-
-      video.current.play();
-    }
-  };
-
-  /** `componentDidMount` */
-  Hook.useDidMount(() => {
-    if (!DOM.isDefined()) return;
-    play();
-  });
-
-  /** `componentDidUpdate` */
-  Hook.useDidUpdate(() => {
-    if (!DOM.isDefined()) return;
-    play();
-  });
-
-  return (
+export const Component = React.forwardRef<HTMLVideoElement, Props.Video>(
+  (
+    { className, isReady, ...divAttrs }: Props.Video,
+    ref
+  ): React.ReactElement => (
     <div {...divAttrs} className={getClassName(className)}>
-      {stream ? (
-        <video className={styles['display']} ref={video} />
+      {isReady ? (
+        <video className={styles['display']} ref={ref} />
       ) : (
         <span>no stream fontawesome icon here</span>
       )}
     </div>
-  );
-};
+  )
+);
 
 /** Sets the component's display name. */
 Component.displayName = 'Video';
